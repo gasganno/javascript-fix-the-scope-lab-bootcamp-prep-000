8159966a48c8123113156fb47ae4581933e3c35c
Jarrod Dyer <jarrod@gasganno.com>
2018-09-11 22:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var animal = 'dog'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var animal = 'dog'
 
 function myAnimal() {
  return animal
@@ -23,7 +50,13 @@ var funkyFunction = function() {
    return "FUNKY!"
  }
 }
-
+*/
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()();
+
+
+
+
+
+
+//var theFunk = funkyFunction()();
